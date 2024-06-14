b251a83786bc1da502cfb61a3bb44ab202b3cdd6
FelipeFVieira <devfelipefranca@gmail.com>
2024-06-13 22:34:17 -0300

    feat: add components

---
+++ src/components/main/homeMain.js
@@ -0,0 +1,12 @@
+import './homeMain.css';
+import React, { useState, useEffect } from 'react';
+
+const HomeMain = () => {
+    return (   
+    <div className='home-main'>
+    
+    </div>
+    )
+}
+
+export default HomeMain;
---
+++ src/components/nav/navbar.js
@@ -0,0 +1,18 @@
+import './navbar.css';
+
+const Navbar = () => {
+    return (   
+    <div className='header'>
+        
+        <div className='logo-div'>
+            <h1>The Band</h1>
+        </div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Bands</a>
+        </nav>
+    </div>
+    )
+}
+
+export default Navbar;
