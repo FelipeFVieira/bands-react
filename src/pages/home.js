21eff10bc1a775f68afc0c1670c36ecf16346786
FelipeFVieira <devfelipefranca@gmail.com>
2024-06-13 22:34:57 -0300

    feat: devoloping pages

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/nav/navbar";
+import HomeMain from "../components/main/homeMain";
+
+const home = () =>{
+    return(
+        <div className="home-container">
+            <Navbar/>
+            <HomeMain/>
+        </div>
+    );
+
+}
